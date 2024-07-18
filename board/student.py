8c69d06d3d13df9520643fbf69b282cc0c5ff9e2
SvenjaCSch <73709681+SvenjaCSch@users.noreply.github.com>
2024-07-18 10:14:31 +0200

    Update student.py

--- board/student.py
+++ board/student.py
@@ -8,7 +8,7 @@ bp = Blueprint("student", __name__)
 def landing():
     return render_template("student/landing.html", name=current_user.name)
 
-@bp.route('/chatbot')
+@bp.route("/chatbot")
 @login_required
-def profile():
-    return render_template('student/chatbot.html', name=current_user.name)
+def chatbot():
+    return render_template("student/chatbot.html", name=current_user.name)
